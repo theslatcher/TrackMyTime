ec7ecb24734a1346f405737d14edf6e5f56fee77
Hpmanen-dev <79310444+Hpmanen-dev@users.noreply.github.com>
2022-04-18 22:35:54 +0200

    Made connection to hosted database

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello World!")
+// Postgres module
+var pg = require('pg');
+var dotenv = require("dotenv")
+
+dotenv.config()
+
+// Database connection
+var conString = process.env.DB_Connection
+var client = new pg.Client(conString);
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  
+    console.log("Connected!");
+})
