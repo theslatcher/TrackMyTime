752700cb2563ef3aaa0129ecbcfe03c4cbd4da71
Marcus <zzoomm-game@hotmail.com>
2022-04-26 19:38:55 +0200

    Serving view depending on if user is logged in.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const path = require('path');
 
 app.use(express.json());
 app.use(require('body-parser').json());
@@ -31,8 +32,13 @@ app.use('/user/', userRouter);
 const timeRouter = require('./routes/time');
 app.use('/time/', timeRouter);
 
+app.use(express.static('views'));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+	if (req.isAuthenticated())
+		res.sendFile(path.join(__dirname, '/views/html/tracker_page.html'));
+	else
+		res.sendFile(path.join(__dirname, '/views/html/homepage.html'));
 });
 
 app.listen(port, () => console.log(`Running on ${port}!`));
