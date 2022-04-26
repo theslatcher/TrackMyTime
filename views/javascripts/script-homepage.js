85fa234b29cd4dc37de3d4c0b9de6a69a1d24f3c
xuan tran <xuandundkl@gmail.com>
2022-04-26 14:30:25 +0200

    apis login and registration to backend

--- views/javascripts/script-homepage.js
+++ views/javascripts/script-homepage.js
@@ -2,6 +2,15 @@ const tabs = document.querySelectorAll("[data-tab-target]")
 const tabContent = document.querySelectorAll("[data-tab-content]");
 const toggleButton = document.getElementsByClassName('toggle-btn')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const signUpForm = document.getElementById('sign-up-form');
+const loginForm = document.getElementById('login-form');
+const loginUserName = document.getElementById('login-username');
+const loginPassword = document.getElementById('login-password');
+const signUpUserName = document.getElementById('sign-up-username');
+const signUpFirstName = document.getElementById('firstName');
+const signUpLastName = document.getElementById('lastName');
+const signUpEmail = document.getElementById('email');
+const signUpPassword = document.getElementById('sign-up-password');
 
 tabs.forEach(tab => {
     tab.addEventListener('click', () => {
@@ -14,7 +23,6 @@ tabs.forEach(tab => {
         })
 
         //show the tab content when click on target tab
-       
         target.classList.add('active');
 
     })
@@ -24,3 +32,53 @@ tabs.forEach(tab => {
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
     })
+
+signUpForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const signUpUser = {
+        username: signUpUserName.value,
+        password: signUpPassword.value,
+        first_name: signUpFirstName.value,
+        last_name: signUpLastName.value,
+        email: signUpEmail.value
+    };
+
+    fetch ('/signup', {
+        method: 'POST',
+        headers: {
+            "Content-Type": 'application/json',
+        },
+        body: JSON.stringify(signUpUser),
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log('Success:', data);
+      })
+    .catch((error) => {
+        console.error('Error:', error);
+      });
+});
+
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const loginUser = {
+        username: loginUserName.value,
+        password: loginPassword.value
+    };
+
+    fetch ('/login', {
+        method: 'POST',
+        headers: {
+            "Content-Type": 'application/json',
+        },
+        body: JSON.stringify(loginUser),
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log('Success:', data);
+      })
+    .catch((error) => {
+        console.error('Error:', error);
+      });
+
+})
