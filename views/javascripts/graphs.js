93c8c304b5e749d01864613f28ec355229b4efc9
kristian <kristian381@hotmail.com>
2022-04-29 13:19:45 +0200

    createpie function

--- views/javascripts/graphs.js
+++ views/javascripts/graphs.js
@@ -4,37 +4,45 @@ const testData = {
   colors: ['#00bcd4', '#ff9800', '#9c27b0', '#009688', '#4caf50', '#795548']
 }
 
-const data = {
-  labels: testData.labels,
-  datasets: [
-    {
-      label: "Summarization of total time in every ",
-      borderColor: testData.colors, // line color
-      backgroundColor: testData.colors, // fill color
-      data: testData.datasets
-    },
-  ],
-};
+const userData = "testuser";
 
-const config = {
-  type: "pie",
-  data: data,
-  options: {
-    responsive: true,
-    title: {
-      display: true,
-      text: "jdfhgkjdfhgjkdf",
-      color: "#fff"
-    },
-    tooltips: {
-      mode: "index",
-      intersect: false,
-    },
-    hover: {
-      mode: "nearest",
-      intersect: true,
-    },
-  },
-};
 
-const myChart = new Chart(document.getElementById("chart-pie-summary"), config);
+function createPie(i) {
+  const data = {
+    labels: testData.labels,
+    datasets: [
+      {
+        label: "Summarization of total time in every ",
+        borderColor: testData.colors, // line color
+        backgroundColor: testData.colors, // fill color
+        data: testData.datasets
+      },
+    ],
+  };
+  
+  const config = {
+    type: "pie",
+    data: data,
+    options: {
+      responsive: true,
+      title: {
+        display: true,
+        text: "jdfhgkjdfhgjkdf",
+        color: "#fff"
+      },
+      tooltips: {
+        mode: "index",
+        intersect: false,
+      },
+      hover: {
+        mode: "nearest",
+        intersect: true,
+      },
+    },
+  };
+  
+  const myChart = new Chart(document.getElementById("chart-pie" + i), config);
+}
+
+document.getElementById("graph-container").innerHTML = '<canvas id="chart-pie1" class="graph-canvas"></canvas>';
+createPie(1);
