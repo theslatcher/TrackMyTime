f75cc85dcf0bfacd7a3bec1bac44eca7d2cd2385
kristian <kristian381@hotmail.com>
2022-05-04 12:38:40 +0200

    Line chart

--- views/javascripts/graphs.js
+++ views/javascripts/graphs.js
@@ -1,20 +1,32 @@
 const testData = {
-  labels: ['Study', 'Games', 'Movies', 'Cooking', 'Workout', 'Sleep'],
+  labels: ["Study", "Games", "Movies", "Cooking", "Workout", "Sleep"],
   datasets: [32, 12, 15, 10, 20, 5],
-  colors: ['#00bcd4', '#ff9800', '#9c27b0', '#009688', '#4caf50', '#795548']
-}
+  colors: ["#00bcd4", "#ff9800", "#9c27b0", "#009688", "#4caf50", "#795548"],
+};
 
-const userData = "testuser";
-
+const userData = "user";
 
 function fetchTrackerTask(user) {
   return new Promise((resolve, reject) => {
     fetch(`/task/user/${user}`)
-      .then(res => res.json())
-      .then(data => {
+      .then((res) => res.json())
+      .then((data) => {
         resolve(data);
       })
-      .catch(err => {
+      .catch((err) => {
+        reject(err);
+      });
+  });
+}
+
+function fetchTimeTracker(id) {
+  return new Promise((resolve, reject) => {
+    fetch(`/time/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        resolve(data);
+      })
+      .catch((err) => {
         reject(err);
       });
   });
@@ -24,7 +36,7 @@ function formatData(data) {
   let labels = [];
   let datasets = [];
   let colors = [];
-  data.forEach(task => {
+  data.forEach((task) => {
     labels.push(task.name);
     datasets.push(task.currenttime);
     colors.push(task.color);
@@ -32,24 +44,45 @@ function formatData(data) {
   return {
     labels: labels,
     datasets: datasets,
-    colors: colors
+    colors: colors,
   };
 }
 
+function formatTime(data) {
+  let labels = [];
+  let datasets = [];
+  let colors = [];
+  console.log(data);
+  let color = "#00bcd4";
+  data.forEach((time) => {
+    labels.push(time.dayofyear);
+    datasets.push(time.totaltime);
+
+
+    colors.push(color);
+  });
+  return {
+    labels: labels,
+    datasets: datasets,
+    colors: colors,
+  };
+}
+
+
 function createPie(d, i, title) {
-  const data = {
+  let data = {
     labels: d.labels,
     datasets: [
       {
         label: "Summarization of total time in every ",
         borderColor: d.colors, // line color
         backgroundColor: d.colors, // fill color
-        data: d.datasets
+        data: d.datasets,
       },
     ],
   };
-  
-  const config = {
+
+  let config = {
     type: "pie",
     data: data,
     options: {
@@ -66,7 +99,7 @@ function createPie(d, i, title) {
         title: {
           display: true,
           text: title,
-          color: '#d3d1d1',
+          color: "#d3d1d1",
           font: {
             size: 18,
           },
@@ -74,13 +107,64 @@ function createPie(d, i, title) {
       },
     },
   };
-  const myChart = new Chart(document.getElementById("chart-pie" + i), config);
+  let myChart = new Chart(document.getElementById("chart-pie" + i), config);
 }
 
-fetchTrackerTask(userData).then(data => {
-  return formatData(data);
-}).then(data => {
-  createPie(data, 1, "Summarization of total time in every task");
+function createLine(d, i, title) {
+  let data = {
+    labels: d.labels,
+    datasets: [
+      {
+        label: "Hours",
+        borderColor: d.colors, // line color
+        backgroundColor: d.colors, // fill color
+        data: d.datasets,
+      },
+    ],
+  };
+
+  let config = {
+    type: "line",
+    data: data,
+    options: {
+      responsive: true,
+      tooltips: {
+        mode: "index",
+        intersect: false,
+      },
+      hover: {
+        mode: "nearest",
+        intersect: true,
+      },
+      plugins: {
+        title: {
+          display: true,
+          text: title,
+          color: "#d3d1d1",
+          font: {
+            size: 18,
+          },
+        },
+      },
+    },
+  };
+  let myChart = new Chart(document.getElementById("chart-pie" + i), config);
+}
+
+fetchTimeTracker("15").then((data) => {
+  return formatTime(data)
+}).then((data) => {
+  createLine(data, 2, "Time spent in each day");
 });
 
-document.getElementById("graph-container").innerHTML = '<canvas id="chart-pie1" class="graph-canvas"></canvas>';
+fetchTrackerTask(userData)
+  .then((data) => {
+    console.log(data);
+    return formatData(data);
+  })
+  .then((data) => {
+    createPie(data, 1, "Summarization of total time in every task");
+  });
+
+document.getElementById("graph-container").innerHTML =
+  '<canvas id="chart-pie1" class="graph-canvas"></canvas><canvas id="chart-pie2" class="graph-canvas"></canvas>';
