a9803c64fba35233e4513a1d8d59b6d8e65ae3b6
Marcus <zzoomm-game@hotmail.com>
2022-05-06 12:25:50 +0200

    Task date querying

--- routes/task.js
+++ routes/task.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const TrackerTask = require("../models/trackerTask")
 const TrackerTime = require("../models/trackerTime")
 const Sequelize = require("sequelize")
+const dayjs = require("dayjs")
+const { Op } = require("sequelize")
 
 router.post("/", async (req, res) => {
     req.body.currenttime = 0;
@@ -35,13 +37,58 @@ router.get("/", async (req, res) => {
     })
 })
 
+function getDateQuery(query) {
+    let dateQuery = {}
+
+    if (query.d)
+	{
+        const day = dayjs(new Date(query.d))
+        const start = day.startOf('day').format()
+        const end = day.endOf('day').format()
+
+		dateQuery = {
+			dayofyear: {
+				[Op.between]: [start, end]
+			}
+		}
+	}
+    else if (query.m)
+    {
+        const month = dayjs(new Date(query.m))
+        const start = month.startOf('month').format()
+        const end = month.endOf('month').format()
+
+		dateQuery = {
+			dayofyear: {
+				[Op.between]: [start, end]
+			}
+		}
+    }
+    else if (query.y)
+    {
+        const year = dayjs(new Date(query.y))
+        const start = year.startOf('year').format()
+        const end = year.endOf('year').format()
+
+		dateQuery = {
+			dayofyear: {
+				[Op.between]: [start, end]
+			}
+		}
+    }
+
+    return dateQuery
+}
+
 router.get("/:id", async (req, res) => {
+	const dateQuery = getDateQuery(req.query)
+	
     await TrackerTask.findOne({
         attributes: {
             include: [[Sequelize.fn("SUM", Sequelize.col("TrackerTime.totaltime")), "currenttime"]]
         },
         where: { trackerid: req.params.id },
-        include: { model: TrackerTime, as: 'TrackerTime', attributes: [] },
+        include: { model: TrackerTime, as: 'TrackerTime', attributes: [], where: dateQuery },
         group: ['TrackerTask.trackerid']
     })
         .then(response => {
@@ -69,12 +116,14 @@ router.delete("/user/:userId", async (req, res) => {
 })
 
 router.get("/user/:userId", async (req, res) => {
+    const dateQuery = getDateQuery(req.query)
+
     await TrackerTask.findAll(({
         attributes: {
             include: [[Sequelize.fn("SUM", Sequelize.col("TrackerTime.totaltime")), "currenttime"]]
         },
         where: { userId: req.params.userId },
-        include: { model: TrackerTime, as: 'TrackerTime', attributes: [] },
+        include: { model: TrackerTime, as: 'TrackerTime', attributes: [], where: dateQuery },
         group: ['TrackerTask.trackerid']
     })).then(response => {
         res.status(200)
