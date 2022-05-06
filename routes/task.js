39c83d322e8204ec816e4fdd81c451e0e5a5e934
nling43 <72392907+nling43@users.noreply.github.com>
2022-05-06 11:28:19 +0200

    get task/user
    makes get task/user/userId add the time for each tracker task

--- routes/task.js
+++ routes/task.js
@@ -11,17 +11,17 @@ router.post("/", async (req, res) => {
         res.send("Success!")
     }).catch(err => {
         res.status(409),
-        res.send({err: err.detail})
+            res.send({ err: err.detail })
     })
 })
 
 router.delete("/", async (req, res) => {
-    await TrackerTask.destroy({where: {trackerid: req.body.trackerid}}).then(response => {
+    await TrackerTask.destroy({ where: { trackerid: req.body.trackerid } }).then(response => {
         res.status(200)
         res.send("Success!")
     }).catch(err => {
         res.status(409)
-        res.send({err: err.detail})
+        res.send({ err: err.detail })
     })
 })
 
@@ -31,48 +31,56 @@ router.get("/", async (req, res) => {
         res.send(response)
     }).catch(err => {
         res.status(404)
-        res.send({err: err.detail})
+        res.send({ err: err.detail })
     })
 })
 
 router.get("/:id", async (req, res) => {
-    await TrackerTask.findOne({attributes: { 
-            include: [[Sequelize.fn("SUM", Sequelize.col("TrackerTime.totaltime")), "currenttime"]] 
-        }, 
-        where: {trackerid: req.params.id}, 
-        include: { model: TrackerTime, as: 'TrackerTime', attributes: [] }, 
-        group: ['TrackerTask.trackerid']})
+    await TrackerTask.findOne({
+        attributes: {
+            include: [[Sequelize.fn("SUM", Sequelize.col("TrackerTime.totaltime")), "currenttime"]]
+        },
+        where: { trackerid: req.params.id },
+        include: { model: TrackerTime, as: 'TrackerTime', attributes: [] },
+        group: ['TrackerTask.trackerid']
+    })
         .then(response => {
             res.status(200)
             res.send(response)
         }).catch(err => {
             res.status(404)
-            res.send({err: err.detail})
+            res.send({ err: err.detail })
         })
 })
 
 router.put("/:id", async (req, res) => {
-    await TrackerTask.update({name: req.body.name, color: req.body.color, goal: req.body.goal},
-        {where: {trackerid: req.params.id}}).then(response => {
-        res.status(200)
-        res.send("Success!")
-    })
+    await TrackerTask.update({ name: req.body.name, color: req.body.color, goal: req.body.goal },
+        { where: { trackerid: req.params.id } }).then(response => {
+            res.status(200)
+            res.send("Success!")
+        })
 })
 
 router.delete("/user/:userId", async (req, res) => {
-	//MW: Should check to see if the correct user is sending the request.
-    TrackerTask.destroy({where: {userId: req.params.userId}}).then(response => {
-		res.sendStatus(200, response)
+    //MW: Should check to see if the correct user is sending the request.
+    TrackerTask.destroy({ where: { userId: req.params.userId } }).then(response => {
+        res.sendStatus(200, response)
     }).catch(err => console.log(err))
 })
 
 router.get("/user/:userId", async (req, res) => {
-    await TrackerTask.findAll({where: {userId: req.params.userId}}).then(response => {
+    await TrackerTask.findAll(({
+        attributes: {
+            include: [[Sequelize.fn("SUM", Sequelize.col("TrackerTime.totaltime")), "currenttime"]]
+        },
+        where: { userId: req.params.userId },
+        include: { model: TrackerTime, as: 'TrackerTime', attributes: [] },
+        group: ['TrackerTask.trackerid']
+    })).then(response => {
         res.status(200)
         res.send(response)
     }).catch(err => console.log(err))
 })
-
 
 
 module.exports = router
