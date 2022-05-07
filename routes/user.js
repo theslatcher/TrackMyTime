412f6556039df640ae77feac5eab7cca477517ec
Marcus <zzoomm-game@hotmail.com>
2022-05-07 17:46:03 +0200

    Moved over the authentication to its own script.

--- routes/user.js
+++ routes/user.js
@@ -1,84 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
-const crypto = require('crypto');
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
-const Sequelize = require('sequelize');
-
-async function hashPassword(password) {
-	return new Promise( (resolve, reject) => {
-		const salt = crypto.randomBytes(64).toString('base64');
-		const iterations = 100000;
-		const hashBytes = 64;
-		crypto.pbkdf2(password, salt, iterations, hashBytes, 'sha256', (err, hash) => {
-			if (err)
-				return reject(err);
-
-			resolve(salt + ':' + hash.toString('base64'));
-		});
-	});
-}
-
-async function verifyPassword(hashedPassword, password) {
-	return new Promise( (resolve, reject) => {
-		const salt = hashedPassword.substring(0,88);
-		const iterations = 100000;
-		const hashBytes = 64;
-		crypto.pbkdf2(password, salt, iterations, hashBytes, 'sha256', (err, hash) => {
-			if (err)
-				return reject(err);
-
-			resolve(hash.toString('base64') == hashedPassword.substring(89));
-		});
-	});
-}
-
-const invalidCredsMessage = 'Incorrect username or password.';
-
-passport.use(new LocalStrategy((username, password, done) => {
-  return User.findOne({where: Sequelize.where(
-		Sequelize.fn('lower', Sequelize.col('username')), 
-		Sequelize.fn('lower', username)
-  	)})
-	.then(async (response) => {
-        if (!response) {
-			return done(null, false, invalidCredsMessage); 
-		}
-
-		validPassword = await verifyPassword(response.password, password);
-
-		if (validPassword)
-			return done(null, {userId: response.userId, username: response.username, first_name: response.first_name,
-						last_name: response.last_name, email: response.email, is_admin: response.username === 'admin'});
-		else
-			return done(null, false, invalidCredsMessage);
-    })
-	.catch(err => {
-        return done(err);
-    });
-}));
-
-passport.serializeUser(function(user, done) {
-	done(null, user);
-});
-
-passport.deserializeUser(function(user, done) {
-	User.findOne({attributes: ['userId', 'username', 'first_name', 'last_name', 'email'], where: {userId: user.userId}})
-	.then(response => {
-		response.is_admin = response.username === 'admin';
-
-        return done(null, response);
-    })
-	.catch(err => {
-        return done(err);
-    });
-});
+const auth = require('../lib/auth');
 
 router.post('/signup', async (req, res) => {
 	if (req.body.password)
-			req.body.password = await hashPassword(req.body.password);
+		req.body.password = await auth.hash_password(req.body.password);
 
 	await User.create(req.body)
 		.then(response => {
@@ -89,7 +17,7 @@ router.post('/signup', async (req, res) => {
 		});
 });
 
-router.post('/login', passport.authenticate('local'), async (req, res) => {
+router.post('/login', auth.authenticate, async (req, res) => {
 	res.cookie('user_details', jwt.sign({user: req.user}, process.env.JWTSecret));
 	res.status(200).send('Success!');
 }); 
@@ -143,7 +71,7 @@ router.delete('/:userId', async (req, res) => {
 				let allowed = req.user.is_admin;
 
 				if (!allowed && req.body.password)
-					allowed = await verifyPassword(response.password, req.body.password);
+					allowed = await auth.verify_password(response.password, req.body.password);
 
 				if (!allowed)
 					return res.status(409).send({error: new Error('Cannot delete your account without providing password.')});
@@ -174,7 +102,7 @@ router.put('/:userId', async (req, res) => {
 
 	User.findOne({where: {userId : req.params.userId}})
 		.then(async (response) => {
-			if (admin_request || await verifyPassword(response.password, req.body.curr_password))
+			if (admin_request || await auth.verify_password(response.password, req.body.curr_password))
 			{
 				let update_fields = {};
 
@@ -191,7 +119,7 @@ router.put('/:userId', async (req, res) => {
 					update_fields['email'] = req.body.email;
 
 				if (req.body.new_password)
-					update_fields['password'] = await hashPassword(req.body.new_password);
+					update_fields['password'] = await auth.hash_password(req.body.new_password);
 
 				User.update(update_fields, {where:{userId: req.params.userId}})
 					.then(response => {
