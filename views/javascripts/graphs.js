4953f52cecbb6c6e0aeaf54ee575cb7fa55ec78c
kristian <kristian381@hotmail.com>
2022-05-12 23:16:47 +0200

    Graph data shows datapoints by day

--- views/javascripts/graphs.js
+++ views/javascripts/graphs.js
@@ -37,12 +37,30 @@ function formatTimeData(data, color, title) {
   let labels = [];
   let datasets = [];
   let colors = [];
-  let newtitle = "Hours spent in " + title;
+  let newtitle = "Summarization of hours spent on " + title;
+
+  let tempTime = 0;
+  let select = 0;
   for (let i = 0; i < data.length; i++) {
-    labels.push(data[i].dayofyear);
-    datasets.push(data[i].totaltime);
-    colors.push(color);
+    tempTime += data[i].totaltime; // add up all the time (for a growing graph)
+    if (labels.includes(data[i].dayofyear)) {
+      // checks if the dates are the same
+      datasets[labels.indexOf(data[i].dayofyear)] += data[i].totaltime;
+    } else {
+      select = i;
+      labels.push(data[i].dayofyear);
+      datasets.push(tempTime);
+      colors.push(color);
+    }
+    console.log(datasets[select]);
   }
+  // if dataset has only one label add 0 to the front of datasets
+  if (labels.length === 1) {
+    datasets.unshift(0);
+    labels.unshift("Start");
+    colors.unshift(color);
+  }
+  console.log(labels, datasets, colors, newtitle);
   return {
     labels: labels,
     datasets: datasets,
@@ -58,8 +76,6 @@ function cfg(type, data, title, label) {
   let scales
   if (document.body.classList.contains('theme-light'))
     color = 'black'
-
-
 
   if (type === 'line')
     scales = {
@@ -77,8 +93,6 @@ function cfg(type, data, title, label) {
       }
     }
 
-
-  console.log(type);
   return {
     type: type,
     data: data,
@@ -152,7 +166,7 @@ async function loadGraphs(user) {
   if (loadedOnce && localStorage.getItem("pieData") && localStorage.getItem("lineData") && localStorage.getItem("trackerData") == JSON.stringify(trackers))
     await createCanvas(JSON.parse(localStorage.getItem("lineData")), JSON.parse(localStorage.getItem("pieData")));
 
-  // arrays to graph data store data
+  // arrays to store graph data
   let pieData = formatData(trackers);
   let lineData = [];
 
